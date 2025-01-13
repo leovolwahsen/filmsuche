35f145df2af6775c2741d5d8b427d5fbbbc818a4
Leo <volwahsenl@gmail.com>
2025-01-13 17:24:52 +0100

    update setup of vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
-    strictPort: true,
+    port: 3000, 
+    strictPort: true, 
   },
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    visualizer({
+      open: true, 
+      gzipSize: true, 
+      brotliSize: true, 
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+          if (id.includes('react')) {
+            return 'react';
+          }
+        },
+      },
+    },
+    cssCodeSplit: true, 
+  },
+});
